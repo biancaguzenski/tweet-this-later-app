ffb73e2d4ad7fbb7a6c5d2fbf5c987114cc10b22
Bianca <biancalway@gmail.com>
2021-02-08 10:57:49 -0300

    Created signup form.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: "main#index"
 
   get "about-us", to: "about#index", as: :about
+  get 'sign_up', to: 'registrations#new'
+  post 'users ', to: 'registrarions#create'
 end
