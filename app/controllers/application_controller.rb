f611eacfc0d2844a04ba41ddf96ed748b70c7418
Bianca <biancalway@gmail.com>
2021-02-09 17:33:18 -0300

    Created login and placed in navbar + model current to set current user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    before_action :set_current_user
+    # before is a method that makes sure set_current_user is done before any action 
+    
+    def set_current_user
+        if session[:user_id]
+            Current.user = User.find_by(id: session[:user_id])
+            # changed this line: @user = User.find_by(id: session[:user_id])
+            # create current model
+        end
+    end
 end
