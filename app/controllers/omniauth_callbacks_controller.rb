68d8fcf23ae61f5fea0f07152bd32369ca253553
Bianca <biancalway@gmail.com>
2021-03-01 11:24:19 -0300

    Added omniauth and twitter authentication.

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,19 @@
+class OmniauthCallbacksController < ApplicationController
+    def twitter
+        # verify if there is already an account for that twitter account
+        twitter_account = Current.user.twitter_accounts.where(username: auth.info.nickname).first_or_initialize
+        twitter_account.update(
+            name: auth.info.name,
+            username: auth.info.nickname,
+            image: auth.info.image,
+            token: auth.credentials.token,
+            secret: auth.credentials.secret,
+        )
+
+        redirect_to twitter_accounts_path, notice: "Successfully connected to your account"
+    end
+
+    def auth
+        request.env['omniauth.auth'] # setted by omniauth gem
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     # virtual atributes: password and password_confirmation - we only 
     # iteract with them when creating a user
-
+    has_many :twitter_accounts
     has_secure_password
 
     validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
